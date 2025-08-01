54e63a87188d558123e63c813b4b1a05bca11528
Daniel <danielgachasut@gmail.com>
2025-08-01 16:05:38 -0500

    Exercise 1.12

--- src/App.jsx
+++ src/App.jsx
@@ -52,9 +52,25 @@ const Statistics = (props) => {
   }
 }
 
+const Anecdotes = (props) => {
+  return(
+    <div>
+      <p>{props.anecdotes}</p>
+    </div>
+  )
+}
 
 const App = () => {
-
+  const anecdotes = ['If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.']
+  
+  const [selected, setSelected] = useState(0)
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -77,6 +93,10 @@ const App = () => {
     console.log('Bad: ', badRev)
     setBad(badRev)
   }
+  const setToSelected = selectedChoice => {
+    console.log('Selected: ' + selectedChoice)
+    setSelected(selectedChoice)
+  }
 
 
   return (
@@ -87,6 +107,8 @@ const App = () => {
       <Button handleClick={() => setToBad(bad + 1)} text="Bad" />
       <Statistics good={good} neutral={neutral} bad={bad} sum={sum}
         aver={aver.toFixed(2)} averGood={averGood.toFixed(2)} />
+      <Anecdotes anecdotes={anecdotes[selected]}/>
+      <Button handleClick={() => setToSelected(Math.floor(Math.random() * 8))} text="Next Anecdote" />
     </div>
   )
 }
